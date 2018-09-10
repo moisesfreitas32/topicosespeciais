2759903b52233956cff80addf3a0ef8b91a0a3fc
Francisco Moisés Bezerra de Freitas <moisesfreitas20@gmail.com>
2018-09-10 20:00:03 -0300

    Projeto Disciplina Topicos

---
+++ ProjetoTopicosespeciais/src/main/java/fvs/edu/br/topicos/ProjetoTopicosespeciaisApplication.java
@@ -0,0 +1,12 @@
+package fvs.edu.br.topicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoTopicosespeciaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoTopicosespeciaisApplication.class, args);
+	}
+}
